ec5038dba0156b666d5e7a325062f2996b6f98ae
Charles Cai <caicongchen@gmail.com>
2020-11-13 22:39:06 -0500

    Day 187 - Added solution for RomanToInteger

--- LeetCode/string/README.md
+++ LeetCode/string/README.md
@@ -5,7 +5,7 @@ Problems:
 | 0006 | [ZigZag Conversion](https://leetcode.com/problems/zigzag-conversion/) | Medium     | 2020-11-9  |           |
 | 0008 | [String to Integer (atoi)](https://leetcode.com/problems/string-to-integer-atoi/) | Medium     | 2020-11-10 |           |
 | 0007 | [Revser Integer](https://leetcode.com/problems/reverse-integer/) | Easy       | 2020-11-12 |           |
-|      |                                                              |            |            |           |
+| 0013 | [Roman to Integer](https://leetcode.com/problems/roman-to-integer/) | Easy       | 2020-11-13 |           |
 |      |                                                              |            |            |           |
 |      |                                                              |            |            |           |
 |      |                                                              |            |            |           |
---
+++ LeetCode/string/RomanToInteger.java
@@ -0,0 +1,57 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class RomanToInteger {
+
+    // bf way;
+    public int romanToInt(String s) {
+        if (s.trim().length() < 1 && s.trim().length() > 15) {
+            return 0;
+        }
+
+        Map<String, Integer> subtraction = new HashMap<>();
+        subtraction.put("IV", 4);
+        subtraction.put("IX", 9);
+        subtraction.put("XL", 40);
+        subtraction.put("XC", 90);
+        subtraction.put("CD", 400);
+        subtraction.put("CM", 900);
+
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int result = 0;
+        for (String key : subtraction.keySet()) {
+            if (s.contains(key)) {
+                result += subtraction.get(key);
+                // Need do the following, String is immutable
+                s = s.replace(key, "");
+            }
+        }
+
+        while (s.trim().length() != 0) {
+            for (char value : s.toCharArray()) {
+                result += map.get(value);
+                int index = s.indexOf(value);
+                s = s.substring(0, index) + s.substring(index + 1);
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        RomanToInteger sol = new RomanToInteger();
+        assert sol.romanToInt("III") == 3 : "III failed";
+        assert sol.romanToInt("IV") == 4 : "IV failed";
+        assert sol.romanToInt("IX") == 9 : "IX failed";
+        assert sol.romanToInt("LVIII") == 58 : "LVIII failed";
+        assert sol.romanToInt("MCMXCIV") == 1994 : "MCMXCIV failed";
+    }
+}
