a09064e5c89c140d04794c59ec70334de64e0ebf
Charles Cai <caicongchen@gmail.com>
2020-12-09 22:13:51 -0500

    Day 213 - Refer to the solution for leetcode 1081 Smallest Subsequence of Distinct Characters

--- LeetCode/string/SmallestSubsequenceofDistinctCharacters.java
+++ LeetCode/string/SmallestSubsequenceofDistinctCharacters.java
@@ -1,9 +1,12 @@
 import java.util.HashSet;
 import java.util.Set;
+import java.util.Stack;
 
 public class SmallestSubsequenceofDistinctCharacters {
 
-    public String smallestSubsequence(String s) {
+    // 1st try
+    // Does not work, because it not guarantee it is subsequence
+    public String smallestSubsequence1st(String s) {
         Set<Character> uniqueChars = new HashSet<>();
 
         for (char c : s.toCharArray()) {
@@ -20,10 +23,43 @@ public class SmallestSubsequenceofDistinctCharacters {
         return sb.toString();
     }
 
+    // 2nd try
+    public String smallestSubsequence(String s) {
+
+        return null;
+    }
+
+    // Best solution
+    public String smallestSubsequenceBest(String S) {
+        Stack<Integer> stack = new Stack<>();
+        int[] last = new int[26], seen = new int[26];
+        for (int i = 0; i < S.length(); ++i)
+            last[S.charAt(i) - 'a'] = i;
+        for (int i = 0; i < S.length(); ++i) {
+            int c = S.charAt(i) - 'a';
+            if (seen[c]++ > 0)
+                continue;
+            while (!stack.isEmpty() && stack.peek() > c && i < last[stack.peek()])
+                seen[stack.pop()] = 0;
+            stack.push(c);
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i : stack)
+            sb.append((char) ('a' + i));
+        return sb.toString();
+    }
+
     public static void main(String[] args) {
         SmallestSubsequenceofDistinctCharacters sol = new SmallestSubsequenceofDistinctCharacters();
+        System.out.println('z' - 'a');
+        System.out.println('A' - 'a');
+        System.err.println(Integer.valueOf('a'));
+        System.err.println(Integer.valueOf('z'));
 
-        assert sol.smallestSubsequence("bcabc").equals("abc") == true : "T1 Failed";
-        assert sol.smallestSubsequence("cbacdcbc").equals("acdb") == true : "T1 Failed";
+        assert sol.smallestSubsequenceBest("cbacdcbc").equals("acdb") == true : "T1 Failed";
+        assert sol.smallestSubsequenceBest("bcabc").equals("abc") == true : "T1 Failed";
+        // why the output must be "acdb" not "cbad" ==>
+        // becasuse "sub-sequence of input string"
+
     }
 }
