f03f397d2918ebbd373c3afd03efe6bb66fa6895
Charles Cai <caicongchen@gmail.com>
2020-12-10 22:21:43 -0500

    Day 214 - add some commet to understand the 1081[Smallest subsequence of distinct characters]

--- LeetCode/string/SmallestSubsequenceofDistinctCharacters.java
+++ LeetCode/string/SmallestSubsequenceofDistinctCharacters.java
@@ -32,17 +32,26 @@ public class SmallestSubsequenceofDistinctCharacters {
     // Best solution
     public String smallestSubsequenceBest(String S) {
         Stack<Integer> stack = new Stack<>();
-        int[] last = new int[26], seen = new int[26];
-        for (int i = 0; i < S.length(); ++i)
+        int[] last = new int[26];
+        int[] seen = new int[26];
+
+        for (int i = 0; i < S.length(); ++i) {
             last[S.charAt(i) - 'a'] = i;
+        }
+
         for (int i = 0; i < S.length(); ++i) {
             int c = S.charAt(i) - 'a';
-            if (seen[c]++ > 0)
+            if (seen[c]++ > 0) {
                 continue;
-            while (!stack.isEmpty() && stack.peek() > c && i < last[stack.peek()])
+            }
+
+            // Trying to understand this part of the code;
+            while (!stack.isEmpty() && stack.peek() > c && i < last[stack.peek()]) {
                 seen[stack.pop()] = 0;
+            }
             stack.push(c);
         }
+
         StringBuilder sb = new StringBuilder();
         for (int i : stack)
             sb.append((char) ('a' + i));
