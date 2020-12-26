0b258ffe16a06549383f6a9fdefbd05f79409c6d
Charles Cai <caicongchen@gmail.com>
2020-12-25 22:20:06 -0500

    added the lc solution

--- LeetCode/string/ReverseInteger.java
+++ LeetCode/string/ReverseInteger.java
@@ -67,6 +67,24 @@ public class ReverseInteger {
         return result * sign;
     }
 
+    // leetcode online solution
+    public int reverseOS(int x) {
+        int rev = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+
+            // "magic number" 7 & -8
+            if (rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && pop > 7))
+                return 0;
+            if (rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && pop < -8))
+                return 0;
+
+            rev = rev * 10 + pop;
+        }
+        return rev;
+    }
+
     public static void main(String[] args) {
         ReverseInteger sol = new ReverseInteger();
 
