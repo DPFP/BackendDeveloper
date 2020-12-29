d390c48aa5a22ab861c2f936aaf5b09bc363155d
Charles Cai <caicongchen@gmail.com>
2020-12-28 23:27:31 -0500

    Day 232 - added leetcode reference solution

--- LeetCode/string/LongestCommonPrefix.java
+++ LeetCode/string/LongestCommonPrefix.java
@@ -48,6 +48,21 @@ public class LongestCommonPrefix {
         return "";
     }
 
+    // LC solution #1 (Horizontal scanning)
+    public String longestCommonPrefixSolOne(String[] strs) {
+        if (strs.length == 0) {
+            return "";
+        }
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++)
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty())
+                    return "";
+            }
+        return prefix;
+    }
+
     public static void main(String[] args) {
         String[] strs = { "flower", "flow", "flight" };
         String[] strs2 = { "dog", "racecar", "car" };
