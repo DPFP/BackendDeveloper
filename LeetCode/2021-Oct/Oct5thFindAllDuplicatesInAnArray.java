7924505250b379bb0cab42f40475fd404fa9e870
Charles Cai <caicongchen@gmail.com>
2021-10-05 21:05:23 -0400

    add my solution and best solution from lc for Oct 5th

---
+++ LeetCode/2021-Oct/Oct5thFindAllDuplicatesInAnArray.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Oct5thFindAllDuplicatesInAnArray {
+
+    public List<Integer> findDuplicates(int[] nums) {
+        Map<Integer, Integer> count = new HashMap<>();
+        List<Integer> res = new ArrayList<>();
+
+        for (int n : nums) {
+            if (count.containsKey(n)) {
+                res.add(n);
+            } else {
+                count.put(n, 1);
+            }
+        }
+
+        return res;
+    }
+
+    // when find a number i, flip the number at position i-1 to negative.
+    // if the number at position i-1 is already negative, i is the number that
+    // occurs twice. (C: this works, because the problem constrains)
+    public List<Integer> findDuplicates2(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < nums.length; ++i) {
+            int index = Math.abs(nums[i]) - 1;
+            if (nums[index] < 0)
+                res.add(Math.abs(index + 1));
+            nums[index] = -nums[index];
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Oct5thFindAllDuplicatesInAnArray sol = new Oct5thFindAllDuplicatesInAnArray();
+        int[] nums = { 1, 1, 2 };
+        for (int i : sol.findDuplicates(nums)) {
+            System.out.print(i + " ");
+        }
+
+        sol.findDuplicates2(nums);
+    }
+}
