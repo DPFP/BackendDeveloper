9f643b5f06b7d7f480494bac5e48d0c865d0b2ce
Charles Cai <caicongchen@gmail.com>
2021-11-01 22:10:18 -0400

    rename the file and added In order traverse

--- LeetCode/binarytree/BinaryTreeTraverse.java
+++ LeetCode/binarytree/BinaryTreeTraverse.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
-public class BinaryTreePreorderTraverse {
+public class BinaryTreeTraverse {
     public List<Integer> preorderTraversal(TreeNode root) {
         // root.val
         // root.left
@@ -21,6 +21,7 @@ public class BinaryTreePreorderTraverse {
         return result;
     }
 
+    // PreOrder Traverse
     // recursive solution
     void preorderTraverse(TreeNode root, List<Integer> value) {
         if (root != null) {
@@ -48,5 +49,32 @@ public class BinaryTreePreorderTraverse {
             }
         }
         return result;
+    }
+
+    // InOrder Traversal
+    // recursive
+    void inroderTraversalR(TreeNode root, List<Integer> res) {
+        if (root != null) {
+            inroderTraversalR(root.left, res);
+            res.add(root.val);
+            inroderTraversalR(root.right, res);
+        }
+    }
+
+    // iteratively
+    List<Integer> inroderTraversalI(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+
+        while (root != null || !stack.empty()) {
+            while (root != null) { // stack could be empty;
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop(); // left
+            res.add(root.val); // root
+            root = root.right; // right
+        }
+        return res;
     }
 }
