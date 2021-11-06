800a13f7ac08505b585349959c44b4676f4fed09
Charles Cai <caicongchen@gmail.com>
2021-11-05 22:18:17 -0400

    further improvement

--- LeetCode/binarytree/PathSum.java
+++ LeetCode/binarytree/PathSum.java
@@ -3,8 +3,8 @@ public class PathSum {
         if (root == null) {
             return false;
         }
-        if (root.left == null && root.right == null && targetSum - root.val == 0) {
-            return true;
+        if (root.left == null && root.right == null) {
+            return targetSum == root.val;
         }
 
         return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
