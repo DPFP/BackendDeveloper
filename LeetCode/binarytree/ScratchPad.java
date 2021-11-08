7518c4aa7daa57a2b5d9e4be534d1f5946549978
Charles Cai <caicongchen@gmail.com>
2021-11-07 21:37:26 -0500

    add a scracth pad

---
+++ LeetCode/binarytree/ScratchPad.java
@@ -0,0 +1,14 @@
+public class ScratchPad {
+    public static void main(String[] args) {
+
+        // Test ++ --
+        int i = 10;
+        System.out.println(i++); // 10
+        i = 10;
+        System.out.println(++i); // 11
+        i = 10;
+        System.out.println(i--); // 10
+        i = 10;
+        System.out.println(--i); // 9
+    }
+}
