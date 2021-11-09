c49a204ee5ae1ae8f505014b607b9fc736e50aad
Charles Cai <caicongchen@gmail.com>
2021-11-08 23:43:44 -0500

    add first try to solve pre and inorder construct

--- LeetCode/binarytree/ConstructBinaryTree.java
+++ LeetCode/binarytree/ConstructBinaryTree.java
@@ -5,6 +5,7 @@ public class ConstructBinaryTree {
 
     int pInorder; // position inorder;
     int pPostorder; // position postorder;
+    int pPreorder; // position preorderr
 
     private TreeNode buildTree(int[] inorder, int[] postorder, TreeNode end) {
         if (pPostorder < 0) {
@@ -37,9 +38,48 @@ public class ConstructBinaryTree {
 
         return buildTree(inorder, postorder, null);
     }
-    // End
+    // Construct Binary Tree from Inorder and Postorder Traversal End
+
+    // Construct Binary Tree from Inorder and Preorder Traversal
+    // recursive method
+    // TODO didn't work :(
+    private TreeNode buildTreeForPreAndIn(int[] preorder, int[] inorder, TreeNode end) {
+        if (pPreorder > preorder.length - 1) {
+            return null;
+        }
+
+        TreeNode root = new TreeNode(preorder[pPreorder++]);
+        if (inorder[pInorder] != root.val) {
+            root.right = buildTreeForPreAndIn(preorder, inorder, root);
+        }
+        pInorder++;
+        if ((end == null) || (inorder[pInorder] != end.val)) {
+            root.left = buildTreeForPreAndIn(preorder, inorder, end);
+        }
+
+        return root;
+    }
+
+    public TreeNode buildTreeForPreAndIn(int[] preorder, int[] inorder) {
+        pPreorder = 0;
+        pInorder = 0;
+
+        return buildTreeForPreAndIn(preorder, inorder, null);
+    }
+    // Construct Binary Tree from Inorder and Preorder Traversal End
 
     public static void main(String[] args) {
+        ConstructBinaryTree sol = new ConstructBinaryTree();
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+
+        int[] inorder = { 9, 3, 15, 20, 7 };
+        int[] postorder = { 9, 15, 7, 20, 3 };
+
+        sol.buildTree(inorder, postorder).inorderTravers(root);
 
     }
 }
