11b63ae49621c82b21ecc90660286146331dfdd7
Charles Cai <caicongchen@gmail.com>
2021-11-09 20:44:44 -0500

    add another more easier understandble recursive method

--- LeetCode/binarytree/ConstructBinaryTree.java
+++ LeetCode/binarytree/ConstructBinaryTree.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 public class ConstructBinaryTree {
 
     // Construct Binary Tree from Inorder and Postorder Traversal
@@ -5,7 +7,6 @@ public class ConstructBinaryTree {
 
     int pInorder; // position inorder;
     int pPostorder; // position postorder;
-    int pPreorder; // position preorderr
 
     private TreeNode buildTree(int[] inorder, int[] postorder, TreeNode end) {
         if (pPostorder < 0) {
@@ -38,9 +39,49 @@ public class ConstructBinaryTree {
 
         return buildTree(inorder, postorder, null);
     }
+
+    // recursive method II -- Easier to understand
+    // (Similar to Python solution from YouTube)
+
+    public TreeNode buildTreePostIn(int[] inorder, int[] postorder) {
+        if (inorder == null || postorder == null || inorder.length != postorder.length) {
+            return null;
+        }
+
+        HashMap<Integer, Integer> hm = new HashMap<>();
+
+        // keep track of index of inorder O(n)
+        for (int i = 0; i < inorder.length; i++) {
+            hm.put(inorder[i], i);
+        }
+
+        return buildTreePostIn(inorder, 0, inorder.length - 1, postorder, 0, postorder.length - 1, hm);
+    }
+
+    private TreeNode buildTreePostIn(int[] inorder, int is, int ie, int[] postorder, int ps, int pe,
+            HashMap<Integer, Integer> hm) {
+        if (ps > pe || is > ie) {
+            return null;
+        }
+
+        TreeNode root = new TreeNode(postorder[pe]);
+        int ri = hm.get(postorder[pe]); // root index
+
+        // left of subtree is from start...rootIndex,
+        root.left = buildTreePostIn(inorder, is, ri - 1, postorder, ps, ps + ri - is - 1, hm);
+
+        // right of subtree is from rootIndex...end
+        root.right = buildTreePostIn(inorder, ri + 1, ie, postorder, ps + ri - is, pe - 1, hm);
+
+        return root;
+    }
+
     // Construct Binary Tree from Inorder and Postorder Traversal End
 
     // Construct Binary Tree from Inorder and Preorder Traversal
+
+    int pPreorder; // position preorderr
+
     // recursive method
     // TODO didn't work :(
     private TreeNode buildTreeForPreAndIn(int[] preorder, int[] inorder, TreeNode end) {
@@ -79,7 +120,9 @@ public class ConstructBinaryTree {
         int[] inorder = { 9, 3, 15, 20, 7 };
         int[] postorder = { 9, 15, 7, 20, 3 };
 
-        sol.buildTree(inorder, postorder).inorderTravers(root);
+        root.inorderTravers(sol.buildTree(inorder, postorder));
+        System.out.println();
+        root.inorderTravers(sol.buildTreePostIn(inorder, postorder));
 
     }
 }
