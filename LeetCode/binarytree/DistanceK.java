ffc581d0739f4d625a665f0bbc4ce138ca2294a0
Charles Cai <caicongchen@gmail.com>
2022-01-09 00:17:47 -0500

    added solution for Distance K

---
+++ LeetCode/binarytree/DistanceK.java
@@ -0,0 +1,121 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+
+public class DistanceK {
+    // 863. All Nodes Distance K in Binary Tree
+    // https://leetcode.com/problems/all-nodes-distance-k-in-binary-tree/
+
+    // initial approach try -- stuck on 2nd step
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        // 1st, BFS find the target
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        int level = 0;
+        boolean found = false;
+
+        while (!q.isEmpty() && !found) {
+            int size = q.size(); // when do we need check the size ?
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = q.poll();
+                if (cur.val == target.val) {
+                    // found the target value
+                    System.out.println("find it " + cur.val + " at level " + level);
+                    found = true;
+                    // 2nd, BFS start from the target to seek - k distance away of nodes (level)
+                    // how to deal with it parent ?
+                }
+                if (cur.left != null) {
+                    q.offer(cur.left);
+                }
+                if (cur.right != null) {
+                    q.offer(cur.right);
+                }
+            }
+            System.out.println(level);
+
+            level++;
+        }
+
+        return res;
+    }
+
+    // 2nd try with suggestion from Labuladong
+    Map<Integer, TreeNode> parent = new HashMap<>();
+
+    public List<Integer> distanceK2(TreeNode root, TreeNode target, int k) {
+        List<Integer> res = new ArrayList<>();
+
+        if (root == null) {
+            return res;
+        }
+
+        // 0, keep track of their parents;
+        traverseParnt(root, null);
+
+        // 1st, BFS find the target
+
+        Queue<TreeNode> q = new LinkedList<>();
+        // KEY noticed here is offer target, not root !!! (this is where it begain)
+        q.offer(target);
+
+        // Missing the visited set;
+        HashSet<Integer> visited = new HashSet<>();
+        visited.add(target.val);
+
+        int level = 0;
+
+        while (!q.isEmpty()) {
+            int size = q.size(); // when do we need check the size ?
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = q.poll();
+
+                if (level == k) {
+                    res.add(cur.val);
+                }
+
+                // grab it parent node
+                TreeNode parentNode = parent.get(cur.val);
+
+                // also add the parent node to the queue
+                if (parentNode != null && !visited.contains(parentNode.val)) {
+                    visited.add(parentNode.val);
+                    q.offer(parentNode);
+                }
+
+                if (cur.left != null && !visited.contains(cur.left.val)) {
+                    visited.add(cur.left.val);
+                    q.offer(cur.left);
+                }
+                if (cur.right != null && !visited.contains(cur.right.val)) {
+                    visited.add(cur.right.val);
+                    q.offer(cur.right);
+                }
+            }
+            System.out.println(level);
+            level++;
+        }
+
+        return res;
+    }
+
+    // pre order traverse
+    private void traverseParnt(TreeNode root, TreeNode parentNode) {
+        if (root == null) {
+            return;
+        }
+
+        parent.put(root.val, parentNode);
+
+        traverseParnt(root.left, root);
+        traverseParnt(root.right, root);
+    }
+}
