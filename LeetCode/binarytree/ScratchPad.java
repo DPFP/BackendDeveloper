938dc580385cf185ef837888a6ef98dc98b44078
CharlesCCC <caicongchen@gmail.com>
2022-01-09 16:36:50 -0500

    added some sort test

--- LeetCode/binarytree/ScratchPad.java
+++ LeetCode/binarytree/ScratchPad.java
@@ -1,3 +1,9 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.PriorityQueue;
+
 public class ScratchPad {
     public static void main(String[] args) {
 
@@ -11,5 +17,26 @@ public class ScratchPad {
         System.out.println(i--); // 10 (print and then minus 1)
         i = 10;
         System.out.println(--i); // 9 (minus 1 and then print)
+
+        // Test priorityQueue
+        System.out.println("--------PQ---Test");
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        pq.add(10);
+        pq.add(9);
+        pq.add(11);
+
+        pq.forEach(x -> System.out.println(x));
+
+        List<Integer> l1 = new ArrayList<Integer>();
+
+        l1.add(10);
+        l1.add(9);
+        l1.add(11);
+
+        l1.forEach(x -> System.out.print(x + " "));
+        System.out.println("--------Sort---Test");
+        Collections.sort(l1);
+        l1.forEach(x -> System.out.print(x + " "));
     }
 }
