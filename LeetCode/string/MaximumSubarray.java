0f927388f9b30b20f6f724d75e8b8d447e7b5485
Charles Cai <caicongchen@gmail.com>
2022-01-26 21:21:01 -0500

    53 Maximum Subarray

--- LeetCode/string/MaximumSubarray.java
+++ LeetCode/string/MaximumSubarray.java
@@ -23,14 +23,14 @@ public class MaximumSubarray {
     // similar apparoch
     public int maxSubArray6(int[] nums) {
         int n = nums.length;
-        int max = Integer.MIN_VALUE, 
+        int max = Integer.MIN_VALUE;
         int sum = 0;
 
         for (int i = 0; i < n; i++) {
             sum += nums[i];
             max = Math.max(sum, max);
 
-            if (sum < 0){
+            if (sum < 0) {
                 sum = 0;
             }
         }
