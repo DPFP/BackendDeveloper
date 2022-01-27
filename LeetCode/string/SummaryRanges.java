83d7954042a7e00e327063ed2a3bd99f51998fcb
Charles Cai <caicongchen@gmail.com>
2022-01-26 23:09:10 -0500

    add more comment

--- LeetCode/string/SummaryRanges.java
+++ LeetCode/string/SummaryRanges.java
@@ -20,17 +20,21 @@ public class SummaryRanges {
         }
 
         for (int i = 0; i < len; i++) {
+            // starting point --> a (current value)
             int a = nums[i];
 
-            // kind like the mountain
+            // kind like the mountain, if it keep including for next
+            // can't replace nums[i] here, but is stil incremeting
             while (i + 1 < len && (nums[i + 1] - nums[i]) == 1) {
                 i++;
             }
 
-            // if a differe then "a -> b" b = nums[i]
+            // nums[i] ==> b (or the End) for current iteration
+            // means i changed. aka end changed
             if (a != nums[i]) {
                 res.add(a + "->" + nums[i]);
             } else {
+                // means i didn't move. end didn't change
                 res.add(a + "");
             }
         }
