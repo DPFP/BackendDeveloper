576848948360a1dc750fdb302717703bb410ac55
Charles Cai <caicongchen@gmail.com>
2022-01-27 23:49:24 -0500

    add solution for valid parentheses

---
+++ LeetCode/string/ValidParentheses.java
@@ -0,0 +1,26 @@
+import java.util.Stack;
+
+public class ValidParentheses {
+
+    public boolean isValid(String s) {
+        if (s.length() % 2 != 0) {
+            // odd length;
+            return false;
+        }
+
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(')');
+            } else if (c == '[') {
+                stack.push(']');
+            } else if (c == '{') {
+                stack.push('}');
+            } else if (stack.isEmpty() || stack.pop() != c) {
+                return false;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
