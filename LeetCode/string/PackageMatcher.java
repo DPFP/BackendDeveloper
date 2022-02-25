779304ef9cfe8c646622b879450e3b7355409169
Charles Cai <caicongchen@gmail.com>
2022-02-24 21:31:38 -0500

    added more comment to help understand the code

--- LeetCode/string/PackageMatcher.java
+++ LeetCode/string/PackageMatcher.java
@@ -8,17 +8,25 @@ import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+//https://leetcode.com/discuss/interview-question/1367405/Shopify-Coding-On-Site-(Package-Matcher)/1083896
 public class PackageMatcher {
 
     private static Map<String, Map<String, Integer>> boxingInfo = new HashMap<>();
 
     static {
+        //- Camera (identifier: Cam): one can fit in a medium box, and up to two can fit in a large box
         Map<String, Integer> cameraBoxing = Stream.of(new Object[][] { { "M", 1 }, { "L", 2 } })
                 .collect(Collectors.toMap(data -> (String) data[0], data -> (Integer) data[1]));
+
+        //- Gaming Console (identifier: Game): too big for a medium box, but up to two can fit in a large box
+        // max of 2 g consoles can fit in 1 box
         Map<String, Integer> gameBoxing = Stream.of(new Object[][] { { "M", 0 }, { "L", 2 } })
                 .collect(Collectors.toMap(data -> (String) data[0], data -> (Integer) data[1]));
+
+        //- Bluetooth speaker (identifier: Blue): one can fit in a large box . max is 1 per large box
         Map<String, Integer> blueBoxing = Stream.of(new Object[][] { { "M", 0 }, { "L", 1 } })
                 .collect(Collectors.toMap(data -> (String) data[0], data -> (Integer) data[1]));
+
         boxingInfo.put("Cam", cameraBoxing);
         boxingInfo.put("Game", gameBoxing);
         boxingInfo.put("Blue", blueBoxing);
@@ -33,8 +41,10 @@ public class PackageMatcher {
 
     private List<Map<String, List<String>>> matchPackages(String[] input) {
         List<Map<String, List<String>>> output = new ArrayList<>();
+
         Map<String, Integer> typeToQuantity = new LinkedHashMap<String, Integer>();
 
+        //calculate all the type and its quantity from input
         Arrays.stream(input).forEach(inp -> {
             if (typeToQuantity.containsKey(inp)) {
                 typeToQuantity.put(inp, typeToQuantity.get(inp) + 1);
@@ -43,6 +53,7 @@ public class PackageMatcher {
             }
         });
 
+        //Loop through all the item, find the matching box info
         for (Map.Entry<String, Integer> entry : typeToQuantity.entrySet()) {
             findPackages(entry.getKey(), entry.getValue(), boxingInfo.get(entry.getKey()), output);
         }
@@ -62,15 +73,16 @@ public class PackageMatcher {
                 values.add(type);
             }
             output.add(Collections.singletonMap("M", values));
-
         } else {
             int count = Math.min(quantity, boxingInfo.get("L"));
-            // fit the required items in large box
+            // fit the required items in large box first
             List<String> values = new ArrayList<String>();
             for (int i = 0; i < count; i++) {
                 values.add(type);
             }
             output.add(Collections.singletonMap("L", values));
+
+            //recursively keep find the matching packages
             findPackages(type, quantity - count, boxingInfo, output);
         }
     }
