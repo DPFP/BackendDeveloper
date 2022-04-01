df2c278d95caec1ab43cdc3f73f26e788d139eac
Charles Cai <caicongchen@gmail.com>
2022-04-01 00:10:36 -0400

    added solution for LC 277

---
+++ Karat/FindtheCelebrity.java
@@ -0,0 +1,37 @@
+public class FindtheCelebrity {
+
+    // 277. Find the Celebrity
+    // https://leetcode.com/problems/find-the-celebrity/
+
+    // YouTube solution
+    public int findCelebrity(int n) {
+        int possibleCeleb = 0;
+        for (int i = 0; i < n; i++) {
+            if (knows(possibleCeleb, i)) {
+                possibleCeleb = i;
+            }
+        }
+        if (isCeleb(possibleCeleb, n)) {
+            return possibleCeleb;
+        }
+        return -1;
+    }
+
+    private boolean isCeleb(int possible, int n) {
+        for (int i = 0; i < n; i++) {
+            if (possible == i) {
+                continue;
+            }
+
+            if (knows(possible, i) || !knows(i, possible)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // from leetcode
+    private boolean knows(int possibleCeleb, int i) {
+        return false;
+    }
+}
