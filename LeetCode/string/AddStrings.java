333d3600bdec3a2160366419f61bbc78a3a6ea5c
Charles Cai <caicongchen@gmail.com>
2022-05-04 20:21:49 -0400

    added LC solution

--- LeetCode/string/AddStrings.java
+++ LeetCode/string/AddStrings.java
@@ -1,8 +1,39 @@
 public class AddStrings {
 
+    // 415. Add Strings
+    // https://leetcode.com/problems/add-strings/
+
+    // LC official solution -- most precise solution
+    public String addStrings(String num1, String num2) {
+        StringBuilder sb = new StringBuilder();
+
+        int carry = 0;
+        int p1 = num1.length() - 1;
+        int p2 = num2.length() - 1;
+
+        while (p1 >= 0 || p2 >= 0) {
+            int x1 = p1 >= 0 ? num1.charAt(p1) - '0' : 0;
+            int x2 = p2 >= 0 ? num2.charAt(p2) - '0' : 0;
+
+            int value = (x1 + x2 + carry) % 10; // digit
+            carry = (x1 + x2 + carry) / 10;
+
+            sb.append(value);
+
+            p1--;
+            p2--;
+        }
+
+        if (carry != 0) {
+            sb.append(carry);
+        }
+
+        return sb.reverse().toString();
+    }
+
     // You must not use any built-in BigInteger library or convert the inputs to
     // integer directly.
-    public String addStrings(String num1, String num2) {
+    public String addStrings2(String num1, String num2) {
         // append the leading zero
         if (num1.length() > num2.length()) {
             num2 = appendZero(num2, num1.length() - num2.length());
@@ -16,8 +47,8 @@ public class AddStrings {
         StringBuilder sb = new StringBuilder();
         int remainder = 0;
         int a, b = 0;
-        for (int i = num1.length() - 1; i >= 0; i--) {
 
+        for (int i = num1.length() - 1; i >= 0; i--) {
             a = Character.getNumericValue(num1.charAt(i));
             b = Character.getNumericValue(num2.charAt(i));
 
@@ -29,12 +60,13 @@ public class AddStrings {
                 remainder = 0;
             }
         }
+
         if (remainder > 0) {
             sb.append(remainder);
         }
         sb.reverse();
 
-        // System.out.println(sb.toString());
+        System.out.println(sb.toString());
 
         return sb.toString();
     }
